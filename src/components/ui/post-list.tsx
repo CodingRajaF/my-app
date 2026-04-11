ac5447c49099aef0e3c958168bbb070f6b96c903
RyoK73 <74401068+CodingRajaF@users.noreply.github.com>
2026-04-11 19:02:51 +0900

    feat:./postsの記事一覧を表示するPostListコンポーネントを実装

--- src/components/ui/post-list.tsx
+++ src/components/ui/post-list.tsx
@@ -1,17 +1,20 @@
 import type { PostData } from "@/lib/post";
+import TagList from "./tag-list";
 import Link from "next/link";
 import path from "path";
 
 // postsで./posts/のコンテンツ一覧を受け取り
-const PostList = (posts: PostData[]) => {
+const PostList = ({ posts }: { posts: PostData[] }) => {
     return (
         <ul>
             {posts.map((post) => {
                 return (
+                    // liはカード形式？かリスト形式にしたい
                     <li key={post.slug} className="block">
                         <Link href={path.join(process.cwd(), post.slug)}>
                             <h1>{post.title}</h1>
                             <time>{post.date}</time>
+                            <TagList tags={post.tag} />
                         </Link>
                     </li>
                 );
