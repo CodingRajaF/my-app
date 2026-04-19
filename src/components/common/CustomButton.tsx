36f4536bd405b0d0c40d0e9ab54fdd8429f31761
RyoK73 <74401068+CodingRajaF@users.noreply.github.com>
2026-04-19 17:50:01 +0900

    feat:挙動を統一するためCustomButtonコンポーネントを実装・反映

---
+++ src/components/common/CustomButton.tsx
@@ -0,0 +1,17 @@
+import React, { Children, useState } from "react";
+
+type ButtonProps = {
+    children: React.ReactNode;
+    onClick: () => void;
+};
+export const CustomButton = ({ children, onClick }: ButtonProps) => {
+    return (
+        <button
+            type="button"
+            onClick={onClick}
+            className="text-3xl p-2 transition hover:scale-120 active:scale-95 duration-500"
+        >
+            {children}
+        </button>
+    );
+};
--- src/components/common/HbMenu.tsx
+++ src/components/common/HbMenu.tsx
@@ -1,15 +1,16 @@
 "use client";
-import { FcMenu } from "react-icons/fc";
+import { LuGrid2X2Plus } from "react-icons/lu";
 import { useState } from "react";
+import { CustomButton } from "./CustomButton";
 
 export const HbMenu = ({ tags, tabs }: { tags: string[]; tabs: string[] }) => {
     const [isOpen, setIsOpen] = useState(true);
     return (
         <div>
             <div className="">
-                <button type="button" className="text-3xl ">
-                    <FcMenu />
-                </button>
+                <CustomButton onClick={() => setIsOpen(!isOpen)}>
+                    <LuGrid2X2Plus />
+                </CustomButton>
                 <div>
                     {tags.map((tag) => {
                         return <></>;
