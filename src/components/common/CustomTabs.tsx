8295d80ecebc06bf73391368adb2fab73120237f
RyoK73 <74401068+CodingRajaF@users.noreply.github.com>
2026-04-19 23:53:52 +0900

    feat:オリジナルTabバーコンポーネントを追加

--- src/components/common/CustomTabs.tsx
+++ src/components/common/CustomTabs.tsx
@@ -7,12 +7,14 @@ export type TabProp = {
 };
 export const CustomTabs = ({ params }: { params: TabProp[] }) => {
     return (
-        <Tabs defaultValue={params[1].tabName}>
+        <Tabs defaultValue={params[0].tabName}>
             <TabsList variant={"line"}>
                 {params.map((param) => {
                     return (
                         <Link key={param.tabName} href={param.tabLink}>
-                            <TabsTrigger value={param.tabName}>{param.tabName}</TabsTrigger>
+                            <TabsTrigger value={param.tabName} className="text-1.5xl">
+                                {param.tabName}
+                            </TabsTrigger>
                         </Link>
                     );
                 })}
