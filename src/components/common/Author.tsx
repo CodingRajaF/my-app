be8a6495f9246d1262910ffdfa8ce574e41e3684
RyoK73 <74401068+CodingRajaF@users.noreply.github.com>
2026-05-17 12:59:25 +0900

    feat:Authorコンポーネントを追加
    テーブル形式で自己ステータスを表示

---
+++ src/components/common/Author.tsx
@@ -0,0 +1,32 @@
+import { CustomCard } from "./CustomCard";
+export const Author = () => {
+    return (
+        <CustomCard label={"Author"}>
+            <p className="text-sm text-foreground leading-relaxed text-left">
+                Web系ソフトウェアエンジニアを志望。
+                <br />
+                開発記録や個人的な技術に対する思いを綴ります。
+            </p>
+            <table className="border-t border-b w-full border-collapse [&_tr]:border-b [&_td]:py-2 [&_td:first-child]:border-r [&_td:first-child]:border-dashed [&_td:last-child]:pl-2 mt-4">
+                <tbody>
+                    <tr className="">
+                        <td className="">FOCUS</td>
+                        <td className="">個人開発/設計</td>
+                    </tr>
+                    <tr>
+                        <td>OS</td>
+                        <td>Arch Linux+Omarchy</td>
+                    </tr>
+                    <tr>
+                        <td>STACK</td>
+                        <td>TypeScript・Next.js</td>
+                    </tr>
+                    <tr>
+                        <td>STATUS</td>
+                        <td>学習中→就活中</td>
+                    </tr>
+                </tbody>
+            </table>
+        </CustomCard>
+    );
+};
