e3849e919ed507455d93304f0ba2058fea2b15e0
RyoK73 <74401068+CodingRajaF@users.noreply.github.com>
2026-05-18 02:11:58 +0900

    fix:未使用import,未実装のコードを削除
    - タグ一覧表示の未実装コンポーネント
    - 別途コンポーネントを作成し、新規branchにて実装検討

--- src/components/common/Sidebar.tsx
+++ src/components/common/Sidebar.tsx
@@ -1,9 +1,5 @@
-import { CustomCard } from "./CustomCard";
-import { TagList } from "./TagList";
-import { getAllPosts } from "@/lib/post";
 import { Author } from "./Author";
 import { cn } from "@/lib/utils";
-const posts = await getAllPosts();
 type SidebarProps = {
     className?: string;
 };
@@ -11,10 +7,6 @@ export const Sidebar = ({ className }: SidebarProps) => {
     return (
         <div className={cn("col-span-1 flex flex-col gap-5 ", className)}>
             <Author />
-            {/* タグ一覧を配置予定 */}
-            {/* <CustomCard title={"Tag"}>
-                <TagList tags={allTags} />
-            </CustomCard> */}
         </div>
     );
 };
